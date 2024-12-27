4879b960066c60741a9cb7cde528d60daf44449b
Myproject <mohamedAlaa-CS>
2024-12-27 23:25:25 +0200

    add hive and init it

--- lib/domin/entities/exam_entity.dart
+++ lib/domin/entities/exam_entity.dart
@@ -1,7 +1,14 @@
+import 'package:hive_flutter/hive_flutter.dart';
+part 'exam_entity.g.dart';
+@HiveType(typeId: 1)
 class ExamEntity {
+  @HiveField(0)
   final String id;
+  @HiveField(1)
   final String title;
+  @HiveField(2)
   final int duration;
+  @HiveField(3)
   final int numberOfQuestions;
 
   ExamEntity({
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:online_exam/core/cache/shared_preferences.dart';
 import 'package:online_exam/core/di/di.dart';
+import 'package:online_exam/domin/entities/exam_entity.dart';
 import 'package:online_exam/online_exam.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   configureDependencies();
   await SharedPreferencesHelper.init();
+  await Hive.initFlutter();
+  Hive.registerAdapter(ExamEntityAdapter());
+  await Hive.openBox<ExamEntity>('exams');
+
   runApp(const OnlineExam());
 }
